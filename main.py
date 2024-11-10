e34d8e932443a26a1674a32abed8c57fddd6257d
gabriel coquillet <gabriel.coquillet@gmail.com>
2024-11-10 17:59:32 +0100

    v0.2 not working rn enemies, shots and score

--- main.py
+++ main.py
@@ -6,6 +6,19 @@ class Ennemi:
     def __init__(self):
         self.ennemi_li = []
 
+    def ennemi_spawn(self):
+        if pyxel.frame_count % 30 == 0:
+            self.ennemi_li.append([random.randint(0,App.WIDTH-8),0])
+
+    def ennemi_move(self):
+        for ennemi in self.ennemi_li:
+            ennemi[1]+=1
+
+    def get_ennemi(self):
+        return self.ennemi_li
+
+    def delete_ennemi(self, ennemi):
+        self.ennemi_li.remove(ennemi)
 
 class Ship:
     def __init__(self):
@@ -14,13 +27,13 @@ class Ship:
     def move(self):
         if pyxel.btn(KEY_ESCAPE):
             pyxel.quit()
-        elif pyxel.btn(pyxel.KEY_Z) and self.y>0:
+        elif pyxel.btn(pyxel.KEY_Z) and self.y>1:
             self.y-=2
-        elif pyxel.btn(pyxel.KEY_S) and self.y<App.HEIGHT:
+        elif pyxel.btn(pyxel.KEY_S) and self.y<App.HEIGHT-7:
             self.y+=2
-        elif pyxel.btn(pyxel.KEY_Q) and self.x>0:
+        elif pyxel.btn(pyxel.KEY_Q) and self.x>1:
             self.x-=2
-        elif pyxel.btn(pyxel.KEY_D) and self.x<App.WIDTH:
+        elif pyxel.btn(pyxel.KEY_D) and self.x<App.WIDTH-7:
             self.x+=2
 
     def getx(self):
@@ -34,7 +47,7 @@ class Shot:
         self.shot_li = []
 
     def add_shot(self, x, y):
-        if pyxel.btnp(pyxel.KEY_SPACE):
+        if pyxel.btnp(pyxel.KEY_SPACE) and not [x,y] in self.shot_li:
             self.shot_li.append([x, y])
 
     def shot_move(self):
@@ -44,26 +57,41 @@ class Shot:
     def get_shot(self):
         return self.shot_li
 
+    def delete_shot(self, shot):
+        self.shot_li.remove(shot)
+
 class App:
     TITLE = "Space Invaders"
-    WIDTH = 300
-    HEIGHT = 400
+    WIDTH = 150
+    HEIGHT = 200
+
     def __init__(self):
         pyxel.init(self.WIDTH, self.HEIGHT, title=self.TITLE)
         pyxel.load("res.pyxres")
+        self.score = 0
         self.ship = Ship()
         self.shots = Shot()
+        self.ennemi1 = Ennemi()
         pyxel.run(self.update,self.draw)
+
+    def score_modify(self):
+        for tir in self.shots.get_shot():
+            for ennemi in self.ennemi1.get_ennemi():
+                pass
 
     def update(self):
         self.ship.move()
         self.shots.add_shot(self.ship.getx(), self.ship.gety())
         self.shots.shot_move()
+        self.ennemi1.ennemi_move()
+        self.ennemi1.ennemi_spawn()
 
     def draw(self):
         pyxel.cls(0)
         pyxel.blt(self.ship.getx(), self.ship.gety(),0,0,0, 8, 8, 0)
         for shot in self.shots.get_shot():
-            pyxel.rect(shot[0], shot[1], 5, 5, 7)
-
+            pyxel.blt(shot[0], shot[1]-3, 0, 0, 8,8,8,0)
+        for ennemi in self.ennemi1.get_ennemi():
+            pyxel.blt(ennemi[0], ennemi[1], 0, 8, 0,8,8,0)
+        pyxel.text(5,5,f"SCORE : {self.score}", 7, None)
 app = App()
